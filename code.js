b43ff854aa064fbab79b202e379fbc2b41da047f
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-03-26 20:17:31 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
+function helper(graph, startNode, nodesVisited = []) {
+    nodesVisited.push(startNode);
+    
+    for (let i = 0; i < graph.length; i++) {
+        if (graph[startNode][i] != 0) {
+            if (nodesVisited.includes(i)) {
+                if (i == startNode) {
+                    return true;
+                }
+            }
+            
+            if (helper(graph, i, nodesVisited)) {
+                return true;
+            }
+        }
+    }
+    nodesVisited.pop();
+    return false;
+}
+
 function hasCycle(graph) {
+    for (let startNode = 0; startNode < graph.length; startNode++) {
+        let cycle = helper(graph, startNode);
+        if (cycle) {
+            return true;
+        }
+    }
     return false;
 }
